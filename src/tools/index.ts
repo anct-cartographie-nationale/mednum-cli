dac6baed33ebed977e537bf492d3614c45866caa
Marc Gavanier <marc.gavanier@gmail.com>
2022-11-10 09:27:53 +0100

    feat: add outputs for hinaura data transformation to data.inclusion schema

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,2 +1,3 @@
+export * from './output';
 export * from './report/report';
 export * from './to-osm-hours/to-osm-hours';
---
+++ src/tools/output/index.ts
@@ -0,0 +1,3 @@
+export * from './file-name/file-name';
+export * from './to-lieux-mediation-numerique-csv/to-lieux-mediation-numerique-csv';
+export * from './write-output-files';
