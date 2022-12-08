01def8a35ae34527059526996c90f24777945308
Marc Gavanier <marc.gavanier@gmail.com>
2022-12-08 12:41:18 +0100

    fix: udpate data inclusion addresse tests

--- src/data-inclusion/fields/adresse/adresse.field.spec.ts
+++ src/data-inclusion/fields/adresse/adresse.field.spec.ts
@@ -27,38 +27,6 @@ describe('adresse field', (): void => {
     expect(commune).toBe('POINTE-A-PITRE');
   });
 
-  it('should replace "," by nothing', (): void => {
-    const dataInclusionVoie: string = 'avenue Gustave Eiffel, cité Générat, Bat K1';
-
-    const voie: string = processVoie(dataInclusionVoie);
-
-    expect(voie).toBe('avenue Gustave Eiffel cité Générat Bat K1');
-  });
-
-  it('should replace "°" by nothing', (): void => {
-    const dataInclusionVoie: string = '43 avenue Faidherbe Bureau N°8';
-
-    const voie: string = processVoie(dataInclusionVoie);
-
-    expect(voie).toBe('43 avenue Faidherbe Bureau N8');
-  });
-
-  it('should replace all "." by nothing', (): void => {
-    const dataInclusionVoie: string = 'Z.A. de la Millière';
-
-    const voie: string = processVoie(dataInclusionVoie);
-
-    expect(voie).toBe('ZA de la Millière');
-  });
-
-  it('should delete all voie after a "|"', (): void => {
-    const dataInclusionVoie: string = '1022 R Antoine de Saint-exupery | R Antoine Saint-exupery';
-
-    const voie: string = processVoie(dataInclusionVoie);
-
-    expect(voie).toBe('1022 R Antoine de Saint-exupery');
-  });
-
   it('should replace all "/" by "-"', (): void => {
     const dataInclusionVoie: string = '2/12 Rue du Chemin des Femmes';
 
@@ -67,36 +35,12 @@ describe('adresse field', (): void => {
     expect(voie).toBe('2-12 Rue du Chemin des Femmes');
   });
 
-  it('should delete all voie after a ":"', (): void => {
-    const dataInclusionVoie: string = '4 Rue Emile Desvaux Adresse :\r\nCentre hospitalier';
-
-    const voie: string = processVoie(dataInclusionVoie);
-
-    expect(voie).toBe('4 Rue Emile Desvaux Adresse Centre hospitalier');
-  });
-
-  it('should trim all voie with brackets', (): void => {
-    const dataInclusionVoie: string = 'Cheminement de la Maison Neuve Noyant (Noyant-la-Gravoyère)';
-
-    const voie: string = processVoie(dataInclusionVoie);
-
-    expect(voie).toBe('Cheminement de la Maison Neuve Noyant');
-  });
-
   it('should trim multiple newlines', (): void => {
     const dataInclusionVoie: string = 'Chemin de la Fauceille\r\nRoute d\'Elne\r\nLieu dit "Mas balande"';
 
     const voie: string = processVoie(dataInclusionVoie);
 
     expect(voie).toBe("Chemin de la Fauceille Route d'Elne Lieu dit Mas balande");
-  });
-
-  it('should replace all long dash to simple dash', (): void => {
-    const dataInclusionVoie: string = '61 – 63 Avenue Paul Krüger';
-
-    const voie: string = processVoie(dataInclusionVoie);
-
-    expect(voie).toBe('61 - 63 Avenue Paul Krüger');
   });
 
   it('should replace "¨" by nothing', (): void => {
@@ -113,22 +57,6 @@ describe('adresse field', (): void => {
     const voie: string = processVoie(dataInclusionVoie);
 
     expect(voie).toBe('16 AV FRANCOIS MITERRAND');
-  });
-
-  it('should replace delete all after "&"', (): void => {
-    const dataInclusionCommune: string = 'Caudebec en Caux & Lillebonne';
-
-    const commune: string = processCommune(dataInclusionCommune);
-
-    expect(commune).toBe('Caudebec en Caux');
-  });
-
-  it('should remove all dot in commune', (): void => {
-    const dataInclusionCommune: string = 'LA POSSESSION B.P. 24';
-
-    const commune: string = processCommune(dataInclusionCommune);
-
-    expect(commune).toBe('LA POSSESSION BP 24');
   });
 
   it('should replace space unicode by regular spaces', (): void => {
@@ -155,27 +83,11 @@ describe('adresse field', (): void => {
     expect(voie).toBe('DISPENSAIRE DPARTEMENTAL 15 RUE BERRY');
   });
 
-  it('should delete all voie after "&"', (): void => {
-    const dataInclusionVoie: string = 'MAISON SOLIDARITÉ & FAMILLES 1 PAS DES ÉCOLES BP 251';
-
-    const voie: string = processVoie(dataInclusionVoie);
-
-    expect(voie).toBe('MAISON SOLIDARITÉ');
-  });
-
   it('should replace any "+" in voie', (): void => {
     const dataInclusionVoie: string = '20 + Place + JEAN BAPTISTE + DURAND';
 
     const voie: string = processVoie(dataInclusionVoie);
 
     expect(voie).toBe('20 Place JEAN BAPTISTE DURAND');
-  });
-
-  it('should delete all voie after ";"', (): void => {
-    const dataInclusionVoie: string = "53 Avenue de l'Europe Ecopolis; 1er étage bureau 09";
-
-    const voie: string = processVoie(dataInclusionVoie);
-
-    expect(voie).toBe("53 Avenue de l'Europe Ecopolis");
   });
 });
--- src/data-inclusion/fields/adresse/adresse.field.ts
+++ src/data-inclusion/fields/adresse/adresse.field.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention, camelcase, no-control-regex, prefer-named-capture-group,no-irregular-whitespace */
 
-export const processVoie = (dataInclusionCommune: string): string =>
-  dataInclusionCommune
+export const processVoie = (dataInclusionVoie: string): string =>
+  dataInclusionVoie
     .replace(/\//gu, '-')
     .replace(/\s/u, ' ')
     .replace(/"/gu, '')
@@ -10,4 +10,13 @@ export const processVoie = (dataInclusionCommune: string): string =>
     .replace(/(\r\n|\n|\r)/gmu, ' ')
     .replace('¨', '')
     .replace('?', 'C')
+    .replace(/\+/gu, '')
+    .replace(/\s+/gu, ' ')
+    .trim();
+
+export const processCommune = (dataInclusionCommune: string): string =>
+  dataInclusionCommune
+    .replace('Ã', 'A')
+    .replace('/', ' sur ')
+    .replace(/\(.*\)/gu, '')
     .trim();
