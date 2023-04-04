8218a597545f396cd03451ad6a44b2b17c5570d9
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-04-04 20:35:55 +0200

    fix: linter

--- src/extract/cli/action/extract.action.ts
+++ src/extract/cli/action/extract.action.ts
@@ -1,35 +1,31 @@
-import { LieuMediationNumerique } from '@gouvfr-anct/lieux-de-mediation-numerique';
-import { ExtractOptions } from '../extract-options';
+/* eslint-disable-next-line @typescript-eslint/no-restricted-imports */
 import * as fs from 'fs';
+import ErrnoException = NodeJS.ErrnoException;
+import { ExtractOptions } from '../extract-options';
 import { SchemaStructureDataInclusionWithServices } from '../../../data-inclusion/main';
 import { createFolderIfNotExist } from '../../../transformer/output/write-output-files';
 
 const filterDataByDepartement = (
   lieuxMediationNumerique: SchemaStructureDataInclusionWithServices[],
   departements: string
-): SchemaStructureDataInclusionWithServices[] => {
-  return lieuxMediationNumerique.filter((lieu: SchemaStructureDataInclusionWithServices) => {
-    const arrayDepartements = departements.split(',');
-    const codePostalDepartement = lieu.code_postal.slice(0, 2);
+): SchemaStructureDataInclusionWithServices[] =>
+  lieuxMediationNumerique.filter((lieu: SchemaStructureDataInclusionWithServices): boolean => {
+    const arrayDepartements: string[] = departements.split(',');
+    const codePostalDepartement: string = lieu.code_postal.slice(0, 2);
     return arrayDepartements.includes(codePostalDepartement);
   });
-};
 
 export const extractAction = (extractOptions: ExtractOptions): void => {
-  const filePath = './assets/input/data-inclusion/data-inclusion.json';
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-    const fullData: SchemaStructureDataInclusionWithServices[] = JSON.parse(data);
-    const filteredLieux: SchemaStructureDataInclusionWithServices[] = filterDataByDepartement(
-      fullData,
+  const filePath: string = './assets/input/data-inclusion/data-inclusion.json';
+  fs.readFile(filePath, 'utf8', (_: ErrnoException | null, data: string): void => {
+    const allLieuxFromDataInclusion: SchemaStructureDataInclusionWithServices[] = JSON.parse(data);
+    const filteredLieuxForExtraction: SchemaStructureDataInclusionWithServices[] = filterDataByDepartement(
+      allLieuxFromDataInclusion,
       extractOptions.departements
     );
     fs.writeFileSync(
       `${createFolderIfNotExist(extractOptions.outputDirectory)}/${extractOptions.sourceName}.json`,
-      JSON.stringify(filteredLieux),
+      JSON.stringify(filteredLieuxForExtraction),
       'utf8'
     );
   });
