fff60acde33b969899c283475721c8129ee2ac26
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-04-27 20:16:09 +0200

    fix: revert

--- src/publier/publish-dataset.ts
+++ src/publier/publish-dataset.ts
@@ -7,7 +7,7 @@ type DatasetToPublishActions = {
   exist?: (datasetToUpdate: PublishDataset, datasetFound: Dataset) => Promise<void>;
 };
 
-// const DATE_FORMAT_CHECK: RegExp = /\d{8}|(\d{4})-(\d{2})-(\d{2})/gu;
+const DATE_FORMAT_CHECK: RegExp = /\d{8}|(\d{4})-(\d{2})-(\d{2})/gu;
 
 const datasetFound = (dataset: Dataset | undefined): dataset is Dataset => dataset != null;
 
@@ -23,7 +23,7 @@ const datasetToPublish =
     await (datasetFound(dataset) ? exist?.(postDataset, dataset) : shouldCreate?.(postDataset));
   };
 
-const removeDate = (title?: string): string | undefined => title?.replace(/\d/gu, '');
+const removeDate = (title?: string): string | undefined => title?.replace(DATE_FORMAT_CHECK, '');
 
 const extractNameFromPath = (splitPath: string[]): string | undefined => splitPath[splitPath.length - 1];
 
