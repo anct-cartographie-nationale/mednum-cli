82ad566716c4013dc78d7dba5e1fc8362fef59a1
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-05-02 19:03:33 +0200

    fix: refactor localisation

--- src/transformer/fields/localisation/localisation.field.ts
+++ src/transformer/fields/localisation/localisation.field.ts
@@ -27,7 +27,7 @@ const checkFormatLocalisation = (localisation: LocalisationToValidate): Localisa
     '+proj=lcc +lat_0=46.5 +lon_0=3 +lat_1=49 +lat_2=44 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs'
   );
   const [longitude, latitude]: number[] = proj4('EPSG:9793', 'EPSG:4326', [localisation.longitude, localisation.latitude]);
-  return latitude && longitude ? Localisation({ latitude, longitude }) : NO_LOCALISATION;
+  return latitude != null && longitude != null ? Localisation({ latitude, longitude }) : NO_LOCALISATION;
 };
 
 const localisationField = (source: DataSource, localisation: Dissociation & Partial<Colonne>): string | undefined =>
