1278111a1fa00d1c4ad470c5bf4f29cbe344455b
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-05-16 17:41:54 +0200

    feat: hinaura missing fields and add typologie

--- src/transformer/fields/pivot/pivot.field.ts
+++ src/transformer/fields/pivot/pivot.field.ts
@@ -1,7 +1,13 @@
 import { Pivot } from '@gouvfr-anct/lieux-de-mediation-numerique';
 import { LieuxMediationNumeriqueMatching, DataSource } from '../../input';
 
-export const processPivot = (source: DataSource, matching: LieuxMediationNumeriqueMatching): Pivot =>
-  matching.pivot?.colonne == null || source[matching.pivot.colonne] === ''
-    ? Pivot('00000000000000')
-    : Pivot(source[matching.pivot.colonne] ?? '00000000000000');
+export const processPivot = (source: DataSource, matching: LieuxMediationNumeriqueMatching): Pivot => {
+  if (matching.pivot?.colonne == null || source[matching.pivot.colonne] === '') {
+    return Pivot('00000000000000');
+  } 
+    try {
+      return Pivot(source[matching.pivot.colonne] ?? '00000000000000');
+    } catch (_) {
+      return Pivot('00000000000000');
+    }
+};
