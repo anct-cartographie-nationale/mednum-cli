418af0b1b817b40dcf9170c72f9acab3665dbd49
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-06-07 17:48:46 +0200

    fix: prettier

--- src/transformer/fields/pivot/pivot.field.ts
+++ src/transformer/fields/pivot/pivot.field.ts
@@ -3,9 +3,9 @@ import { LieuxMediationNumeriqueMatching, DataSource } from '../../input';
 
 export const processPivot = (source: DataSource, matching: LieuxMediationNumeriqueMatching): Pivot => {
   try {
-    const colonne:string = matching.pivot?.colonne ?? '';
+    const colonne: string = matching.pivot?.colonne ?? '';
     return Pivot(source[colonne] ?? '00000000000000');
   } catch {
     return Pivot('00000000000000');
   }
-}
+};
