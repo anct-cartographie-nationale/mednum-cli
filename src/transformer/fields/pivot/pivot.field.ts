f6daa7fdcd430b4986a9576c0bd95dc5bf99a4a4
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-06-07 17:42:32 +0200

    fix: process pivot factorisation

--- src/transformer/fields/pivot/pivot.field.ts
+++ src/transformer/fields/pivot/pivot.field.ts
@@ -2,12 +2,10 @@ import { Pivot } from '@gouvfr-anct/lieux-de-mediation-numerique';
 import { LieuxMediationNumeriqueMatching, DataSource } from '../../input';
 
 export const processPivot = (source: DataSource, matching: LieuxMediationNumeriqueMatching): Pivot => {
-  if (matching.pivot?.colonne == null || source[matching.pivot.colonne] === '') {
-    return Pivot('00000000000000');
-  }
   try {
-    return Pivot(source[matching.pivot.colonne] ?? '00000000000000');
-  } catch (_) {
+    const colonne = matching.pivot?.colonne ?? '';
+    return Pivot(source[colonne] ?? '00000000000000');
+  } catch {
     return Pivot('00000000000000');
   }
-};
+}
