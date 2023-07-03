8d092f01774cf0753013340f45ea3d992a99db2c
Marc Gavanier <marc.gavanier@gmail.com>
2023-07-03 10:15:00 +0200

    feat: remove cnfs duplication rule (#97)

--- src/dedupliquer/cli/action/remove-duplicates/duplication-rules.ts
+++ src/dedupliquer/cli/action/remove-duplicates/duplication-rules.ts
@@ -14,20 +14,12 @@ const selectDuplication =
   (lieuDuplication: SchemaLieuMediationNumerique, lieu: SchemaLieuMediationNumerique): Duplication =>
     lieuDuplication.date_maj < tooOld.toISOString() ? { id: lieuDuplication.id } : { id: lieuDuplication.id, mergeId: lieu.id };
 
-const cnfsDuplicationRule =
-  (tooOld: Date) =>
-  (lieu1: SchemaLieuMediationNumerique, lieu2: SchemaLieuMediationNumerique): Duplication => {
-    if (lieu1.source === 'conseiller-numerique') return selectDuplication(tooOld)(lieu2, lieu1);
-    if (lieu2.source === 'conseiller-numerique') return selectDuplication(tooOld)(lieu1, lieu2);
-    return {};
-  };
-
 const dateDuplicationRule =
   (tooOld: Date) =>
   (lieu1: SchemaLieuMediationNumerique, lieu2: SchemaLieuMediationNumerique): Duplication =>
     lieu1.date_maj < lieu2.date_maj ? selectDuplication(tooOld)(lieu1, lieu2) : selectDuplication(tooOld)(lieu2, lieu1);
 
-const DUPLICATION_RULES: DuplicationRule[] = [cnfsDuplicationRule, dateDuplicationRule];
+const DUPLICATION_RULES: DuplicationRule[] = [dateDuplicationRule];
 
 export const applyDuplicationRules =
   (tooOld: Date) =>
