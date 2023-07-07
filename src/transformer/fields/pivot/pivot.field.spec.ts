e213f693b026cb64dc632e788c8abfb4dc187cdf
Marc Gavanier <marc.gavanier@gmail.com>
2023-07-07 12:17:33 +0200

    fix: handle pivot with number type (#103)

--- src/transformer/fields/pivot/pivot.field.spec.ts
+++ src/transformer/fields/pivot/pivot.field.spec.ts
@@ -21,6 +21,22 @@ describe('pivot field', (): void => {
     expect(pivot).toBe('12345678910111');
   });
 
+  it('should get pivot from data source using matching information for number pivot', (): void => {
+    const matching: LieuxMediationNumeriqueMatching = {
+      pivot: {
+        colonne: 'Siret'
+      }
+    } as LieuxMediationNumeriqueMatching;
+
+    const source: DataSource = {
+      Siret: 17230001400013 as unknown as string
+    };
+
+    const pivot: Pivot = processPivot(source, matching);
+
+    expect(pivot).toBe('17230001400013');
+  });
+
   it('should get dummy pivot when ther is no match', (): void => {
     const matching: LieuxMediationNumeriqueMatching = {
       pivot: {
