48ec65ded941bd05ef377ac17632e3cd234feaf1
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-07-17 18:20:24 +0200

    fix: refacto doublons in same source

--- src/transformer/cli/action/duplicates-same-source/duplicates-same-source.ts
+++ src/transformer/cli/action/duplicates-same-source/duplicates-same-source.ts
@@ -1,20 +1,27 @@
 import { LieuMediationNumerique } from '@gouvfr-anct/lieux-de-mediation-numerique';
 import { ratio } from 'fuzzball';
 
-export const keepOneEntryPerSource = (data: LieuMediationNumerique[]): LieuMediationNumerique[] => {
-  const uniqueEntries: LieuMediationNumerique[] = data.filter(
-    (entry: LieuMediationNumerique, index: number, array: LieuMediationNumerique[]): boolean =>
-      array.findIndex((item: LieuMediationNumerique): boolean => {
-        const isSameSource: boolean = item.source === entry.source;
-        const isSimilarNom: boolean = ratio(item.nom, entry.nom) > 90;
-        const isSimilarAdresse: boolean = ratio(item.adresse as unknown as string, entry.adresse as unknown as string) > 90;
-        const isSameLocation: boolean =
-          item.localisation?.latitude === entry.localisation?.latitude &&
-          item.localisation?.longitude === entry.localisation?.longitude;
+const isSameSource = (structure: LieuMediationNumerique, lieu: LieuMediationNumerique): boolean =>
+  structure.source === lieu.source;
 
-        return isSameSource && isSimilarNom && isSimilarAdresse && isSameLocation;
-      }) === index
+const isSimilarNom = (structure: LieuMediationNumerique, lieu: LieuMediationNumerique): boolean =>
+  ratio(structure.nom, lieu.nom) > 90;
+
+const isSimilarAdresse = (structure: LieuMediationNumerique, lieu: LieuMediationNumerique): boolean =>
+  ratio(structure.adresse as unknown as string, lieu.adresse as unknown as string) > 90;
+
+const isSameLocation = (structure: LieuMediationNumerique, lieu: LieuMediationNumerique): boolean =>
+  structure.localisation?.latitude === lieu.localisation?.latitude &&
+  structure.localisation?.longitude === lieu.localisation?.longitude;
+
+export const keepOneEntryPerSource = (data: LieuMediationNumerique[]): LieuMediationNumerique[] =>
+  data.filter(
+    (lieu: LieuMediationNumerique, index: number, array: LieuMediationNumerique[]): boolean =>
+      array.findIndex(
+        (structure: LieuMediationNumerique): boolean =>
+          isSameSource(structure, lieu) &&
+          isSimilarNom(structure, lieu) &&
+          isSimilarAdresse(structure, lieu) &&
+          isSameLocation(structure, lieu)
+      ) === index
   );
-
-  return uniqueEntries;
-};
