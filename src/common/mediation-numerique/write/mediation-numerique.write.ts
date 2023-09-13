2d13b67961d2d2dc71072056e3108a5b85aa860a
Marc Gavanier <marc.gavanier@gmail.com>
2023-09-13 18:08:44 +0200

    Feat/import lieux mediation numerique in db (#123)
    * feat: generate lieux mediation numerique file to import
    
    * feat: add publish to database step in deduplicate job

--- src/common/mediation-numerique/write/mediation-numerique.write.ts
+++ src/common/mediation-numerique/write/mediation-numerique.write.ts
@@ -1,6 +1,6 @@
 /* eslint-disable-next-line @typescript-eslint/no-restricted-imports */
 import * as fs from 'fs';
-import { SchemaLieuMediationNumerique } from '@gouvfr-anct/lieux-de-mediation-numerique';
+import { fromSchemaLieuxDeMediationNumerique, SchemaLieuMediationNumerique } from '@gouvfr-anct/lieux-de-mediation-numerique';
 import { createFolderIfNotExist, noEmptyCell, Output, throwWriteFileError } from '../../output-file';
 import { mediationNumeriqueFileName } from '../file-name/mediation-numerique.file-name';
 import { mediationNumeriqueToCsv } from '../to-csv/mediation-numerique.to-csv';
@@ -37,6 +37,17 @@ export const writeMediationNumeriqueCsvOutput = (
       suffix
     )}`,
     mediationNumeriqueToCsv(schemaLieuxDeMediationNumerique),
+    throwWriteFileError
+  );
+};
+
+export const writeMediationNumeriqueDynamoDBJsonOutput = (
+  producer: Output,
+  schemaLieuxDeMediationNumerique: SchemaLieuMediationNumerique[]
+): void => {
+  fs.writeFile(
+    `${createFolderIfNotExist(producer.path)}/LieuxInclusionNumerique.json`,
+    JSON.stringify(fromSchemaLieuxDeMediationNumerique(schemaLieuxDeMediationNumerique).slice(0, 100), noEmptyCell),
     throwWriteFileError
   );
 };
