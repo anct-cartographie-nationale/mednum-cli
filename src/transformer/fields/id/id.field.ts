e5cf5dffa037f344952c0bce4e64c03f937b977f
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-12-21 14:45:30 +0100

    fix: take index as id if source is missing (#164)
    * fix: take index as id if source is missing
    
    * fix: linter

--- src/transformer/fields/id/id.field.ts
+++ src/transformer/fields/id/id.field.ts
@@ -1,5 +1,11 @@
 import { Id } from '@gouvfr-anct/lieux-de-mediation-numerique';
-import { LieuxMediationNumeriqueMatching, DataSource } from '../../input';
+import { LieuxMediationNumeriqueMatching, DataSource, Colonne } from '../../input';
+
+const idIsEmptyInSource = (source: DataSource, id: Colonne): boolean => source[id.colonne] == null || source[id.colonne] === '';
 
 export const processId = (source: DataSource, matching: LieuxMediationNumeriqueMatching, index: number): Id =>
-  Id((matching.id == null ? index.toString() : source[matching.id.colonne]?.toString()) ?? index.toString());
+  Id(
+    (matching.id == null || idIsEmptyInSource(source, matching.id)
+      ? index.toString()
+      : source[matching.id.colonne]?.toString()) ?? index.toString()
+  );
