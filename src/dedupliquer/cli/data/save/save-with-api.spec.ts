f95984d9716e5f3385562c778ca8a4f33ab32b56
Marc Gavanier <marc.gavanier@gmail.com>
2024-01-10 20:16:37 +0100

    fix: update id source separator (#168)

--- src/dedupliquer/cli/data/save/save-with-api.spec.ts
+++ src/dedupliquer/cli/data/save/save-with-api.spec.ts
@@ -18,7 +18,7 @@ describe('save merged lieux with API', (): void => {
     const mergeGroupsMap: Map<string, string[]> = new Map<string, string[]>([
       [
         '4ee6737afbdef63ac81397e16c10ac24518e028dca130889c44ba28db446270d',
-        ['Hinaura@05a12890-03e5-5abd-949c-cbaaf5b9afe5', 'Hinaura@96cfc8b3-94e0-48a1-bb90-bd324e9535b2']
+        ['Hinaura_05a12890-03e5-5abd-949c-cbaaf5b9afe5', 'Hinaura_96cfc8b3-94e0-48a1-bb90-bd324e9535b2']
       ]
     ]);
 
@@ -31,7 +31,7 @@ describe('save merged lieux with API', (): void => {
     const mergeGroupsMap: Map<string, string[]> = new Map<string, string[]>([
       [
         '4ee6737afbdef63ac81397e16c10ac24518e028dca130889c44ba28db446270d',
-        ['Res-in@05a12890-03e5-5abd-949c-cbaaf5b9afe5', 'Res-in@96cfc8b3-94e0-48a1-bb90-bd324e9535b2']
+        ['Res-in_05a12890-03e5-5abd-949c-cbaaf5b9afe5', 'Res-in_96cfc8b3-94e0-48a1-bb90-bd324e9535b2']
       ]
     ]);
 
