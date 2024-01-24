564fe5618fa32dbc181a6c9e09d780ad33116d20
Marc Gavanier <marc.gavanier@gmail.com>
2024-01-24 16:24:38 +0100

    feat: add extractions (#173)

--- src/extract/cli/extract-options.ts
+++ src/extract/cli/extract-options.ts
@@ -1,18 +1,26 @@
 import { Command } from 'commander';
 import { Question } from 'inquirer';
-import { departementsOption, outputDirectoryOption, sourceNameOption } from './options';
-import { departementsQuestion, outputDirectoryQuestion, sourceNameQuestion } from './questions';
+import { departementsOption, outputDirectoryOption, sourceNameOption, territoryOption } from './options';
+import { departementsQuestion, outputDirectoryQuestion, sourceNameQuestion, territoryQuestion } from './questions';
 
 export type ExtractOptions = {
   outputDirectory: string;
   sourceName: string;
   departements: string;
+  territory: string;
+  cartographieNationaleApiUrl: string;
 };
 
-export const EXTRACT_OPTIONS: ((program: Command) => Command)[] = [outputDirectoryOption, sourceNameOption, departementsOption];
+export const EXTRACT_OPTIONS: ((program: Command) => Command)[] = [
+  outputDirectoryOption,
+  sourceNameOption,
+  departementsOption,
+  territoryOption
+];
 
 export const extractOptionsQuestions = (extractOptions: ExtractOptions): Question[] => [
   outputDirectoryQuestion(extractOptions),
   sourceNameQuestion(extractOptions),
-  departementsQuestion(extractOptions)
+  departementsQuestion(extractOptions),
+  territoryQuestion(extractOptions)
 ];
