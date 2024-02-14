ef560f1cd7971fc1139824c83775dd8def91d13a
Marc Gavanier <marc.gavanier@gmail.com>
2024-02-14 04:26:12 +0100

    feat: lieux with duplicates from api are paginated (#190)

--- src/dedupliquer/cli/action/dedupliquer.action.ts
+++ src/dedupliquer/cli/action/dedupliquer.action.ts
@@ -1,6 +1,5 @@
-import axios, { AxiosResponse } from 'axios';
 import { SchemaLieuMediationNumerique } from '@gouvfr-anct/lieux-de-mediation-numerique';
-
+import { paginate } from '../../../common';
 import { DeduplicationRepository } from '../../repositories';
 import {
   DuplicationComparison,
@@ -21,27 +20,39 @@ const onlyMoreThanDuplicationScoreThreshold =
   (duplicationComparison: DuplicationComparison): boolean =>
     duplicationComparison.score > (allowInternalMerge ? INTERNAL_DUPLICATION_SCORE_THRESHOLD : DUPLICATION_SCORE_THRESHOLD);
 
-/* eslint-disable-next-line max-statements */
+/* eslint-disable-next-line max-statements, max-lines-per-function */
 export const dedupliquerAction = async (dedupliquerOptions: DedupliquerOptions): Promise<void> => {
-  const repository: DeduplicationRepository = deduplicationRepository(dedupliquerOptions);
+  try {
+    const repository: DeduplicationRepository = deduplicationRepository(dedupliquerOptions);
 
-  const allLieuxWithDuplicates: AxiosResponse<SchemaLieuMediationNumerique[]> = await axios.get(dedupliquerOptions.baseSource);
+    const allLieuxWithDuplicates: SchemaLieuMediationNumerique[] = await paginate<SchemaLieuMediationNumerique>(
+      `${dedupliquerOptions.baseSource.split('?')[0]}?page[number]=0&page[size]=10000`,
+      dedupliquerOptions.baseSource.split('?')[1]
+    );
 
-  const lieuxToDeduplicate: AxiosResponse<SchemaLieuMediationNumerique[]> = await axios.get(dedupliquerOptions.source);
+    const lieuxToDeduplicate: SchemaLieuMediationNumerique[] = await paginate<SchemaLieuMediationNumerique>(
+      `${dedupliquerOptions.source.split('?')[0]}?page[number]=0&page[size]=10000`,
+      dedupliquerOptions.source.split('?')[1]
+    );
 
-  const duplicationComparisonsToGroup: DuplicationComparison[] = duplicationComparisons(
-    allLieuxWithDuplicates.data,
-    dedupliquerOptions.allowInternal,
-    lieuxToDeduplicate.data
-  ).filter(onlyMoreThanDuplicationScoreThreshold(dedupliquerOptions.allowInternal));
+    const duplicationComparisonsToGroup: DuplicationComparison[] = duplicationComparisons(
+      allLieuxWithDuplicates,
+      dedupliquerOptions.allowInternal,
+      lieuxToDeduplicate
+    ).filter(onlyMoreThanDuplicationScoreThreshold(dedupliquerOptions.allowInternal));
 
-  const groups: Groups = groupDuplicates(duplicationComparisonsToGroup);
-  const merged: MergedLieuxByGroupMap = mergeDuplicates(new Date())(allLieuxWithDuplicates.data, groups);
+    const groups: Groups = groupDuplicates(duplicationComparisonsToGroup);
 
-  /* eslint-disable-next-line no-console */
-  console.log('Nouveaux lieux concernés par une fusion :', groups.itemGroupMap.size);
-  /* eslint-disable-next-line no-console */
-  console.log('Nouveaux lieux fusionnés à enregistrer :', merged.size);
+    const merged: MergedLieuxByGroupMap = mergeDuplicates(new Date())(allLieuxWithDuplicates, groups);
 
-  await repository.save(groups, merged, allLieuxWithDuplicates.data);
+    /* eslint-disable-next-line no-console */
+    console.log('Nouveaux lieux concernés par une fusion :', groups.itemGroupMap.size);
+    /* eslint-disable-next-line no-console */
+    console.log('Nouveaux lieux fusionnés à enregistrer :', merged.size);
+
+    await repository.save(groups, merged, allLieuxWithDuplicates);
+  } catch (error) {
+    /* eslint-disable-next-line no-console */
+    console.log(error);
+  }
 };
---
+++ src/extract/cli/action/build-api-url/extract-query-string.spec.ts
@@ -0,0 +1,28 @@
+import { extractQueryString } from './extract-query-string';
+
+describe('build api url', (): void => {
+  it('should build url with filter on code insee', (): void => {
+    const url: string = extractQueryString({
+      departements: '01,03,07,15,26,38,42,43,63,69,73,74',
+      duplicates: true
+    });
+
+    expect(url).toBe('and[mergedIds][exists]=false&adresse[beginsWith][code_insee]=01,03,07,15,26,38,42,43,63,69,73,74');
+  });
+
+  it('should build url without departements', (): void => {
+    const url: string = extractQueryString({
+      duplicates: true
+    });
+
+    expect(url).toBe('and[mergedIds][exists]=false');
+  });
+
+  it('should build url without duplicates', (): void => {
+    const url: string = extractQueryString({
+      duplicates: false
+    });
+
+    expect(url).toBe('');
+  });
+});
