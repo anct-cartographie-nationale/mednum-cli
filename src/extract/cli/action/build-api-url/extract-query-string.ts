e57a44cdea239f051cda3ec95609b78bf0cbe9ad
Marc Gavanier <marc.gavanier@gmail.com>
2024-02-15 01:52:35 +0100

    fix: add missing extract all merged ids and group filters

--- src/extract/cli/action/build-api-url/extract-query-string.ts
+++ src/extract/cli/action/build-api-url/extract-query-string.ts
@@ -4,6 +4,6 @@ export const extractQueryString = ({
   departements,
   duplicates
 }: Pick<ExtractOptions, 'departements' | 'duplicates'>): string => {
-  const mergedIds: string = duplicates ? 'and[mergedIds][exists]=false' : '';
+  const mergedIds: string = duplicates ? 'and[mergedIds][exists]=false' : 'or[mergedIds][exists]=true&or[group][exists]=false';
   return departements == null ? mergedIds : `${mergedIds}&adresse[beginsWith][code_insee]=${departements}`;
 };
