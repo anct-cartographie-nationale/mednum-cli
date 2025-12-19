635ae849905e898902334be868890db728ab0587
Marc Gavanier <marc@gavanier.com>
2025-12-19 10:33:39 +0100

    fix: exclude duplicated lieux from le havre (#321)

--- src/dedupliquer/cli/action/deduplication.repository.ts
+++ src/dedupliquer/cli/action/deduplication.repository.ts
@@ -19,5 +19,18 @@ export const deduplicationRepository = (dedupliquerOptions: DedupliquerOptions):
     }
   },
   isIncluded: (lieu: SchemaLieuMediationNumerique): boolean =>
-    excludeById(['Coop-numérique_fb1fa128-9b9a-49e2-8f19-3c9751c80f68'])(lieu)
+    excludeById([
+      'Coop-numérique_fb1fa128-9b9a-49e2-8f19-3c9751c80f68',
+      'France-Services_1819',
+      'dora_bf60f3b4-d0d0-4f40-90e3-a999507dc070',
+      'dora_ee2dc0e5-3d35-4dcb-9202-cc4df05f9ccd',
+      'France-Services_1817',
+      'France-Services_452',
+      'dora_04361a45-4926-4760-a409-605dc71e3025',
+      'France-Services_2506',
+      'France-Services_2826',
+      'dora_04b66816-f68a-404e-a083-216d9318c380',
+      'dora_95174f8f-ed02-4b4c-93b7-b644a4ca838f',
+      'dora_b67d0721-da50-412d-8b3f-b962bdf65bcf'
+    ])(lieu)
 });
